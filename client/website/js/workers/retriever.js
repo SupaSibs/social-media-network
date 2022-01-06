d6102949ea4e96b9787514fd17216a07f7c5c267
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-01-06 18:41:42 +0200

    idk

--- client/website/js/workers/retriever.js
+++ client/website/js/workers/retriever.js
@@ -1,9 +1,10 @@
 "use strict";
 onmessage = (posts, users, elementsDOM) => {
-for (const element of elementsDOM)
+    //iterates through the element dom (an array of elements) and inserts the
+for (const element of elementsDOM) {
 let rand = Math.floor(Math.random() * array.element);
 let randIndex = posts[rand];
 element.querySelector(".body") = elementsDOM.body
-element.querySelector(".author").querySelector(".text") = randIndex.author.text;
-element.querySelector(".author").querySelector(".image") = randIndex.author.image
- }
+element.querySelector(".author").querySelector(".text") = randIndex.author;
+element.querySelector(".author").querySelector(".image") = users.randIndex.author.image
+ } }
